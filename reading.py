162e7526662bc38394ae71a7a2f8c2fa2ae4f7da
Rishabh-14 <77384078+Rishabh-14@users.noreply.github.com>
2023-09-13 10:53:23 +0530

    reading.py

--- reading.py
+++ reading.py
@@ -1,5 +1,15 @@
 # import library
+from email import message
+import smtplib
+from email.message import EmailMessage
 
 # initialize variable -> login -> select
+msg['arjunmalhotra3@gmail.com','acrobat@13']
+msg.select('inbox')
 
-# search -> split -> fetch -> email message
+# search -> split -> fetch -> email messagemsg.search
+satatus, message.numbers = mail.search(None,'ALL')
+for num in message_numbers[0].split():
+    status, msg_data = mail.fetch(num,'RFS')
+    email_message = message_from_bytes[0]
+[1]
